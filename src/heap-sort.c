82053543ebe73b788bdf25b943612d69f5b05bc6
ecarney <carneyedwardj@gmail.com>
2021-02-27 17:52:52 -0500

    Updates output printing to attempt to add runtime.

---
+++ src/heap-sort.c
@@ -0,0 +1,7 @@
+int heapSort(double * inputArray, int numElements) {
+
+}
+
+int maxHeapify() {
+    
+}
--- src/main.c
+++ src/main.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
+#include <time.h>
 #include "utilities.h"
 #include "selection-sort.h"
 #include "insertion-sort.h"
 
 const char * inputFile = ".\\..\\data\\array-input-1.txt";
+clock_t clockTime;
+double runtime;
 
 int testInsertionSort();
+int testSelectionSort();
 
 int main() {
     int ret = 0;
 
-
     ret += testInsertionSort();
+    ret += testSelectionSort();
 
-
-    return 0;
+    return ret < 0 ? -1 : 0;
 }
 
 int testInsertionSort() {
     double* unsortedArray;
     int numElements;
-    printf("\n");
+    
+    printf("\nINSERTION SORT\n");
     if (readArrayFromFile(inputFile, &unsortedArray, &numElements) < 0) {
         printf("ERROR: Could not read array from file: %s\n", inputFile);
         return -1;
@@ -28,8 +32,33 @@ int testInsertionSort() {
 
     printDoubleArray(unsortedArray, numElements);
 
+    clockTime = clock();
     if (insertionSort(unsortedArray, numElements) < 0) {
         printf("ERROR: Problem doing Insertion Sort\n");
+        return -1;
+    }
+    clockTime = clock() - clockTime;
+    runtime = clockTime * 1000 / CLOCKS_PER_SEC;
+
+    printDoubleArray(unsortedArray, numElements);
+    printf("Total time taken (ms): %f", runtime);
+    printf("\n");
+    return 0;
+}
+
+int testSelectionSort() {
+    double* unsortedArray;
+    int numElements;
+    printf("\nSELECTION SORT\n");
+    if (readArrayFromFile(inputFile, &unsortedArray, &numElements) < 0) {
+        printf("ERROR: Could not read array from file: %s\n", inputFile);
+        return -1;
+    }
+
+    printDoubleArray(unsortedArray, numElements);
+
+    if (selectionSort(unsortedArray, numElements) < 0) {
+        printf("ERROR: Problem doing Selection Sort\n");
         return -1;
     }
 
