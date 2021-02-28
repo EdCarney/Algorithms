02bbbe76209ba6dffa938e6b2ec1fea77bd71437
ecarney <carneyedwardj@gmail.com>
2021-02-28 12:22:49 -0500

    Adds initial implementation for treaps (tree-heaps).

---
+++ src/treap-sort.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+
+struct tnode {
+    char key;
+    int priority;
+    struct tnode* parent;
+    struct tnode* lnode;
+    struct tnode* rnode;
+};
+
+// Inserts a new node into a treap.
+// Returns a pointer to the root node of the treap.
+struct tnode* treapInsert(struct tnode* rootNode, struct tnode* newNode);
+
+struct tnode* treapInsert(struct tnode* rootNode, struct tnode* newNode) {
+    
+    // if root node is null, no treap exists yet
+    if (rootNode == NULL)
+        return newNode;
+
+
+    return rootNode;
+}
