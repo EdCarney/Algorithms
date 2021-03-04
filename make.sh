64d7c705d84cf02f4d2b5b909cf65aad5a64daf1
ecarney <carneyedwardj@gmail.com>
2021-03-04 18:03:55 -0500

    Makes some updates and adds  a bash file for building/running on linux.

---
+++ make.sh
@@ -0,0 +1,20 @@
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+# remove old files
+rm -f bin/*
+rm -f *.exe
+echo "Removed old compiled files"
+
+# call make
+make
+
+# run main
+pushd "./bin"
+./main.exe
+popd
