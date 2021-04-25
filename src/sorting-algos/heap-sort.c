f6d6eb954838a0665b8578161fe1b760ea2d6058
ecarney <carneyedwardj@gmail.com>
2021-04-25 15:33:47 -0400

    Adds heap-sort to this branch.

---
+++ src/sorting-algos/heap-sort.c
@@ -0,0 +1,102 @@
+#include "heap-sort.h"
+
+struct heap* initializeHeap() {
+    struct heap* heap = malloc(sizeof(struct heap));
+    heap->heapSize = 0;
+    heap->length = 0;
+    heap->values = NULL;
+    return heap;
+}
+
+struct heap* initializeHeapFromArray(double inputArray[], int numElements) {
+    struct heap* heap = initializeHeap();
+    heap->values = (double*)calloc(numElements, sizeof(double));
+    heap->length = numElements;
+    for (int i = 0; i < numElements; ++i)
+        heap->values[i] = inputArray[i];
+    return heap;
+}
+
+int calcParentNode(int i) {
+    return (int)floor(i / 2);
+}
+
+int calcLeftNode(int i) {
+    return (2 * (i + 1)) - 1;
+}
+
+int calcRightNode(int i) {
+    return 2 * (i + 1);
+}
+
+void minHeapSort(struct heap *heap) {
+    buildMinHeap(heap);
+    for (int i = heap->length - 1; i > 0; --i) {
+        double temp = heap->values[0];
+        heap->values[0] = heap->values[i];
+        heap->values[i] = temp;
+        --(heap->heapSize);
+        minHeapify(heap, 0);
+    }
+}
+
+void maxHeapSort(struct heap *heap) {
+    buildMaxHeap(heap);
+    for (int i = heap->length - 1; i > 0; --i) {
+        double temp = heap->values[0];
+        heap->values[0] = heap->values[i];
+        heap->values[i] = temp;
+        --(heap->heapSize);
+        maxHeapify(heap, 0);
+    }
+}
+
+void maxHeapify(struct heap* heap, int index) {
+    int left = calcLeftNode(index);
+    int right = calcRightNode(index);
+    int largest = index;
+
+    if (left < heap->heapSize && heap->values[left] > heap->values[largest])
+        largest = left;
+    
+    if (right < heap->heapSize && heap->values[right] > heap->values[largest])
+        largest = right;
+
+    if (largest != index) {
+        double temp = heap->values[index];
+        heap->values[index] = heap->values[largest];
+        heap->values[largest] = temp;
+        maxHeapify(heap, largest);
+    }
+}
+
+void minHeapify(struct heap* heap, int index) {
+    int left = calcLeftNode(index);
+    int right = calcRightNode(index);
+    int smallest = index;
+
+    if (left < heap->heapSize && heap->values[left] < heap->values[smallest])
+        smallest = left;
+    
+    if (right < heap->heapSize && heap->values[right] < heap->values[smallest])
+        smallest = right;
+
+    if (smallest != index) {
+        double temp = heap->values[index];
+        heap->values[index] = heap->values[smallest];
+        heap->values[smallest] = temp;
+        minHeapify(heap, smallest);
+    }
+}
+
+struct heap* buildMaxHeap(struct heap* heap) {
+    heap->heapSize = heap->length;
+    for (int i = (int)floor((heap->length) / 2); i >= 0; --i)
+        maxHeapify(heap, i);
+}
+
+struct heap* buildMinHeap(struct heap* heap) {
+    heap->heapSize = heap->length;
+    for (int i = (int)floor((heap->length) / 2); i >= 0; --i)
+        minHeapify(heap, i);
+}
